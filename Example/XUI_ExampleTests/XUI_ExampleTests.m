4c85fffb49634e46bd9d248aa6ae0424cdcdec81
Lessica <i.82@me.com>
2017-10-12 13:21:53 +0800

    Add tests to pass Travis CI

---
+++ Example/XUI_ExampleTests/XUI_ExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  XUI_ExampleTests.m
+//  XUI_ExampleTests
+//
+//  Created by Zheng Wu on 12/10/2017.
+//  Copyright © 2017 Lessica. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XUI_ExampleTests : XCTestCase
+
+@end
+
+@implementation XUI_ExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
